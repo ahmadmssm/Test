243b3c4f4f526bdd77787578c48f22421f3a6e5f
Ahmad Mahmoud <ams.eng@hotmail.com>
2022-12-29 19:46:00 +0200

    Fix

--- .git-hooks/install-git-hooks.bash
+++ .git-hooks/install-git-hooks.bash
@@ -10,7 +10,7 @@ do
 	SCRIPT_NAME=$(basename "$SCRIPT_PATH")
 	if [ $SCRIPT_NAME != install-git-hooks.bash ]; then
 		SCRIPT_NAME_WITHOUT_EXTENSION=${SCRIPT_NAME%.*}
-		TO_PATH=$GIT_DIR/hooks/$SCRIPT_NAME_WITHOUT_EXTENSION
+		TO_PATH=$GIT_DIR/hooks/$SCRIPT_NAME
 		echo "➡️ Copy From => $SCRIPT_PATH"
 		echo "➡️ Copy To => $TO_PATH"
 		ln -sf "$SCRIPT_PATH" "$TO_PATH"
